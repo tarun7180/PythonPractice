2a8cc6c4daaf1d599be7bd20f2ddf4be213c835c
tarun7180 <mailtarunjain@gmail.com>
2024-05-22 23:28:01 +0530

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 22:37:00 2024
+
+@author: Tarun
+"""
+
+from array import *
+
+strSampl = 'python'
+print(strSampl)
+
+listNumbers = [1,2,3,4,5,5,5,5]
+
+print(listNumbers)
+
+listMixed = [1,2,3,4,'tarun','j',7]
+print(listMixed)
+
+sampleArray = array('i',[1,2,3,4])
+print(sampleArray)
+
+for i in sampleArray : print(i)
+
+sampleTuple = (1,2,3,3,2,1,'tj','ab')
+print(sampleTuple)
