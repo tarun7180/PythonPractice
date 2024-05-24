f5e5aee164295775d033b85c4c1bfcf8dac373cc
tarun7180 <mailtarunjain@gmail.com>
2024-05-24 12:58:31 +0530

    Create Indexing.py

---
+++ Python for Data Science-NPTEL/Indexing.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 24 12:18:57 2024
+
+@author: Tarun
+"""
+
+# String: Indexing
+sampleString = 'leaning'
+
+sampleString.index('l')
+
+sampleString.index('ning')
+
+sampleString[7]
+
+sampleString[-2]
+
+sampleString[-9]
